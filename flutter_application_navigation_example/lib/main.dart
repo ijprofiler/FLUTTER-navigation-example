22d7745d64f11ff8845f4e97b5353ade7eb6acb1
ijprofiler <ijprofiler@gmail.com>
2024-07-12 23:54:24 +0300

    First

---
+++ flutter_application_navigation_example/lib/main.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Routing and Navigation'),
+          centerTitle: true,
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Route route = MaterialPageRoute(builder: (context) => Page2());
+          Navigator.push(context, route);
+        },
+        child: Text('Move to Page 2'),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 2'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Go Back')),
+      ),
+    );
+  }
+}
